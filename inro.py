07886da4a7f20a37f6f5ee201a6aad56576c7591
MuhammadRizkyPramadhanaLubis <lubisr002@gmail.com>
2024-09-03 12:39:35 +0700

    Perkuliahan 1

---
+++ inro.py
@@ -0,0 +1,24 @@
+# Deklaasi Variabel
+a = float(10.5)
+b = 5
+c = a + b
+
+# Operasi (Operation) -> tambah (+), kurang(-), bagi(/), kali(*), modulo(%), pangkat (**)
+print(a)
+print(b)
+print(c)
+
+
+print(" ")
+
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
